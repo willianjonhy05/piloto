a8c4699d7a3fa362f01faab4897d26532e83a1cd
willianjonhy05 <willianjohny05@gmail.com>
2023-10-21 21:14:12 -0300

    Meu projetinho pessoal

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function criarBase() {
+    fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+    fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+}
+
+criarBase()
